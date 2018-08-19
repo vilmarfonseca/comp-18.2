63ad6e02bd5284d63455196085a3e4983a7fcc91
Vilmar Fonseca <vilmarfonseca@Vilmars-MBP.home>
2018-08-19 01:24:40 -0300

    Começando a fazer a parte do Hash e mais organizações gerais

---
+++ Etapa 01/hash.h
@@ -0,0 +1,14 @@
+#define HASH_SIZE 997
+
+typedef struct hash_node
+{
+    int type;
+    char *text;
+    struct hash_node *next;
+}   HASH_NODE;
+
+void hashInit(void);
+int hashAddress(char *text);
+HASH_NODE* hashInsert(int type, char *text);
+HASH_NODE* hashFind(char *text);
+void hashPrint(void);
