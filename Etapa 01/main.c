b5ef247ff0ce5d1baa2dbc54c48437c943ebdc46
Vilmar Fonseca <vilmarfonseca@Vilmars-MBP.home>
2018-08-20 00:42:41 -0300

    Arrumando bug dos comentários

--- Etapa 01/main.c
+++ Etapa 01/main.c
@@ -1,7 +1,7 @@
 
 int main (int argc, char **argv){
     
-    int tok;
+    int tok = 0;
 
 	if (argc < 2) 
 	{
@@ -18,83 +18,89 @@ int main (int argc, char **argv){
 	init();
 	
     while(isRunning()){
+
         tok = yylex();
+		if(!isRunning())
+			break;
 
         switch(tok){
 			case KW_INT:
-				fprintf(stderr, "Found KW_INT - line %i\n", getLineNumber());
+				fprintf(stderr, "Found KW_INT on line %i\n", getLineNumber());
 				break;
 			case KW_FLOAT:
-				fprintf(stderr, "Found KW_FLOAT - line %i\n", getLineNumber());
+				fprintf(stderr, "Found KW_FLOAT on line %i\n", getLineNumber());
 				break;
 			case KW_CHAR:
-				fprintf(stderr, "Found KW_CHAR - line %i\n", getLineNumber());
+				fprintf(stderr, "Found KW_CHAR on line %i\n", getLineNumber());
 				break;
 			case KW_IF:
-				fprintf(stderr, "Found KW_IF - line %i\n", getLineNumber());
+				fprintf(stderr, "Found KW_IF on line %i\n", getLineNumber());
 				break;
 			case KW_THEN:
-				fprintf(stderr, "Found KW_THEN - line %i\n", getLineNumber());
+				fprintf(stderr, "Found KW_THEN on line %i\n", getLineNumber());
 				break;
 			case KW_ELSE:
-				fprintf(stderr, "Found KW_ELSE - line %i\n", getLineNumber());
+				fprintf(stderr, "Found KW_ELSE on line %i\n", getLineNumber());
 				break;
 			case KW_WHILE:
-				fprintf(stderr, "Found KW_WHILE - line %i\n", getLineNumber());
+				fprintf(stderr, "Found KW_WHILE on line %i\n", getLineNumber());
 				break;
 			case KW_READ:
-				fprintf(stderr, "Found KW_READ - line %i\n", getLineNumber());
+				fprintf(stderr, "Found KW_READ on line %i\n", getLineNumber());
 				break;
 			case KW_PRINT:
-				fprintf(stderr, "Found KW_PRINT - line %i\n", getLineNumber());
+				fprintf(stderr, "Found KW_PRINT on line %i\n", getLineNumber());
 				break;
 			case KW_RETURN:
-				fprintf(stderr, "Found KW_RETURN - line %i\n", getLineNumber());
+				fprintf(stderr, "Found KW_RETURN on line %i\n", getLineNumber());
 				break;
 			case OPERATOR_LE:
-				fprintf(stderr, "Found OPERATOR_LE - line %i\n", getLineNumber());
+				fprintf(stderr, "Found OPERATOR_LE on line %i\n", getLineNumber());
 				break;
 			case OPERATOR_GE:
-				fprintf(stderr, "Found OPERATOR_GE - line %i\n", getLineNumber());
+				fprintf(stderr, "Found OPERATOR_GE on line %i\n", getLineNumber());
 				break;
 			case OPERATOR_EQ:
-				fprintf(stderr, "Found OPERATOR_EQ - line %i\n", getLineNumber());
+				fprintf(stderr, "Found OPERATOR_EQ on line %i\n", getLineNumber());
 				break;
 			case OPERATOR_AND:
-				fprintf(stderr, "Found OPERATOR_AND - line %i\n", getLineNumber());
+				fprintf(stderr, "Found OPERATOR_AND on line %i\n", getLineNumber());
 				break;
 			case OPERATOR_OR:
-				fprintf(stderr, "Found OPERATOR_OR - line %i\n", getLineNumber());
+				fprintf(stderr, "Found OPERATOR_OR on line %i\n", getLineNumber());
 				break;
 			case OPERATOR_NOT:
-				fprintf(stderr, "Found OPERATOR_NOT - line %i\n", getLineNumber());
+				fprintf(stderr, "Found OPERATOR_NOT on line %i\n", getLineNumber());
 				break;
 			case LIT_INTEGER:
-				fprintf(stderr, "Found LIT_INTEGER - line %i\n", getLineNumber());
+				fprintf(stderr, "Found LIT_INTEGER on line %i\n", getLineNumber());
 				break;
 			case LIT_FLOAT:
-				fprintf(stderr, "Found LIT_FLOAT - line %i\n", getLineNumber());
+				fprintf(stderr, "Found LIT_FLOAT on line %i\n", getLineNumber());
 				break;
 			case LIT_CHAR:
-				fprintf(stderr, "Found LIT_CHAR - line %i\n", getLineNumber());
+				fprintf(stderr, "Found LIT_CHAR on line %i\n", getLineNumber());
 				break;
 			case LIT_STRING:
-				fprintf(stderr, "Found LIT_STRING - line %i\n", getLineNumber());
+				fprintf(stderr, "Found LIT_STRING on line %i\n", getLineNumber());
 				break;
 			case TK_IDENTIFIER:
-				fprintf(stderr, "Found TK_IDENTIFIER - line %i\n", getLineNumber());
+				fprintf(stderr, "Found TK_IDENTIFIER on line %i\n", getLineNumber());
 				break;
 			case TOKEN_ERROR:
-				fprintf(stderr, "Found TOKEN_ERROR - line %i\n", getLineNumber());
+				fprintf(stderr, "Found TOKEN_ERROR on line %i\n", getLineNumber());
 				break;
 			default:
-				fprintf(stderr, "Found %c - line %i\n", tok, getLineNumber());
+				fprintf(stderr, "Found %c on line %i\n", tok, getLineNumber());
 				break;
 		}
     }
+
+	printf("\n\n-----------------------HASH TABLE-----------------------\n");
 	
 	hashPrint();
 	
+	running = 0;
 	return 1;
 
 }
