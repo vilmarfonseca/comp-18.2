b789dc4d660373002136c010c4e702b3f738c005
Vilmar Fonseca <vilmarfonseca@Vilmars-MBP.home>
2018-10-21 15:48:29 -0300

    Adicionando funções vistas em aula

--- Etapa 04/semantic.c
+++ Etapa 04/semantic.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "semantic.h"
+
+
+void checkOperands(AST_NODE *node){
+    int  i;
+    
+    if (node == 0)
+        return;
+    for (i = 0; i<MAX_SONS; i++)
+        checkOperands(node->son[i]);
+}
--- Etapa 04/semantic.h
+++ Etapa 04/semantic.h
@@ -2,5 +2,12 @@
 #define semantic_h
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "hash.h"
+#include "ast.h"
+
+void setDeclaration(AST_NODE *node);
+void checkUndeclared();
+void checkOperands(AST_NODE *node);
 
 #endif /* semantic_h */
