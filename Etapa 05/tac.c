d8b9d19a45fcb5f0c3b659680f67160b3eb4fa3b
Vilmar Fonseca <vilmarfonseca@Vilmars-MBP.home>
2018-11-12 22:56:01 -0200

    Quase funcionando, segfault com arquivo de exemplo

--- Etapa 05/tac.c
+++ Etapa 05/tac.c
@@ -10,8 +10,7 @@
 #include <stdarg.h>
 #include "tac.h"
 
-TAC *tacCreate(int type, HASH_NODE *res, HASH_NODE *op1, HASH_NODE *op2)
-{
+TAC *tacCreate(int type, HASH_NODE *res, HASH_NODE *op1, HASH_NODE *op2){
     TAC *new;
     new = (TAC *) calloc(1, sizeof(TAC));
     new->type = type;
@@ -23,37 +22,30 @@ TAC *tacCreate(int type, HASH_NODE *res, HASH_NODE *op1, HASH_NODE *op2)
     return new;
 }
 
-void tacPrintPrev(TAC* node)
-{
-    if(node)
-    {
+void tacPrintPrev(TAC* node){
+    if(node){
         tacPrintNode(node);
         tacPrintPrev(node->prev);
     }
 }
 
-void tacPrintNext(TAC* node)
-{
-    if(node)
-    {
+void tacPrintNext(TAC* node){
+    if(node){
         tacPrintNode(node);
         tacPrintNext(node->next);
     }
 }
 
-TAC *tacReverse(TAC *ultimo)
-{
+TAC *tacReverse(TAC *ultimo){
     TAC *tac;
-    for(tac=ultimo; tac->prev; tac = tac->prev)
-    {
+    for(tac=ultimo; tac->prev; tac = tac->prev){
         tac->prev->next = tac;
     }
     
     return tac;
 }
 
-TAC *tacJoin(TAC *tac1, TAC *tac2)
-{
+TAC *tacJoin(TAC *tac1, TAC *tac2){
     TAC *iterator;
     if (!tac1)
         return tac2;
@@ -67,8 +59,7 @@ TAC *tacJoin(TAC *tac1, TAC *tac2)
     return tac2;
 }
 
-TAC *tacGenerate(AST_NODE *node)
-{
+TAC *tacGenerate(AST_NODE *node){
     int i = 0;
     TAC *code[4];
     TAC *result;
@@ -76,66 +67,44 @@ TAC *tacGenerate(AST_NODE *node)
     if(!node)
         return 0;
     
-    if(node->type == AST_FUNCTION)
-        return makeFnCall(node);
-    
-    for(i = 0; i < 4; ++i)
-    {
+    for(i = 0; i < 4; ++i){
         code[i] = tacGenerate(node->sons[i]);
     }
     
-    switch(node->type)
-    {
+    switch(node->type){
             
-        case AST_IDENTIFIER:    result = tacCreate(AST_IDENTIFIER, node->symbol, 0, 0); break;
-            
-            /* GLOBAIS */
-        case AST_VARIABLE:   result = makeVariavel(node->symbol, code);  break;
-            
-        case AST_ARRAY_EMPTY:   result = makeArray(node->symbol, code); break;
-        case AST_LIT_SEQUENCE:  result = makeArrayInicializado(node->sons[0]->symbol, code);    break;
-            
-            /* FUNÇÕES */
-        case AST_RETURN:    result = makeReturn(code);  break;
-        case AST_FUNCTION:  result = makeFnDef(node->symbol, code); break;
-            
-            /* PRINT, READ, ETC */
-        case AST_READ:  result = makeRead(node->symbol);    break;
-        case AST_PRINT:    result = makePrint(code);   break;
-            
-            /* OPERADORES ARITMÉTICOS */
-        case AST_ADD:   result = makeOperacaoBinaria(TAC_ADD, code);    break;
-        case AST_SUB:   result = makeOperacaoBinaria(TAC_SUB, code);    break;
-        case AST_MULT:  result = makeOperacaoBinaria(TAC_MULT, code);   break;
-        case AST_DIV:   result = makeOperacaoBinaria(TAC_DIV, code);    break;
-            
-            /* OPERADORES LÓGICOS */
-        case AST_AND:   result = makeOperacaoBinaria(TAC_AND, code);    break;
-        case AST_OR:    result = makeOperacaoBinaria(TAC_OR,  code);    break;
-        case AST_LE:    result = makeOperacaoBinaria(TAC_LE,  code);    break;
-        case AST_GE:    result = makeOperacaoBinaria(TAC_GE,  code);    break;
-        case AST_EQ:    result = makeOperacaoBinaria(TAC_EQ,  code);    break;
-        case AST_NOT:   result = makeOperacaoBinaria(TAC_NOT, code);    break;
-        case AST_LESS:  result = makeOperacaoBinaria(TAC_LESS, code);   break;
-            
-            /* ATRIBUIÇÃO */
-        case AST_ATTR_SINGLE:   result = makeAtribuicao(node->symbol, code);    break;
-        case AST_ATTR_ARRAY:    result = makeAtribuicaoArray(node->symbol, code);   break;
-            
-        default:    result = tacJoin(tacJoin(tacJoin(code[0], code[1]), code[2]) , code[3]);
+        case AST_IDENTIFIER:        result = tacCreate(AST_IDENTIFIER, node->symbol, 0, 0); break;
+        case AST_VARIABLE:          result = makeVariavel(node->symbol, code);  break;  
+        case AST_ARRAY_EMPTY:       result = makeArray(node->symbol, code); break;
+        case AST_LIT_SEQUENCE:      result = makeArrayInicializado(node->sons[0]->symbol, code);    break;
+        case AST_RETURN:            result = makeReturn(code);  break;
+        case AST_FUNCTION:          result = makeFnDef(node->symbol, code); break;
+        case AST_READ:              result = makeRead(node->symbol);    break;
+        case AST_PRINT:             result = makePrint(code);   break;
+        case AST_ADD:               result = makeOperacaoBinaria(TAC_ADD, code);    break;
+        case AST_SUB:               result = makeOperacaoBinaria(TAC_SUB, code);    break;
+        case AST_MULT:              result = makeOperacaoBinaria(TAC_MULT, code);   break;
+        case AST_DIV:               result = makeOperacaoBinaria(TAC_DIV, code);    break;
+        case AST_AND:               result = makeOperacaoBinaria(TAC_AND, code);    break;
+        case AST_OR:                result = makeOperacaoBinaria(TAC_OR,  code);    break;
+        case AST_LE:                result = makeOperacaoBinaria(TAC_LE,  code);    break;
+        case AST_GE:                result = makeOperacaoBinaria(TAC_GE,  code);    break;
+        case AST_EQ:                result = makeOperacaoBinaria(TAC_EQ,  code);    break;
+        case AST_NOT:               result = makeOperacaoBinaria(TAC_NOT, code);    break;
+        case AST_LESS:              result = makeOperacaoBinaria(TAC_LESS, code);   break;
+        case AST_ATTR_SINGLE:       result = makeAtribuicao(node->symbol, code);    break;
+        case AST_ATTR_ARRAY:        result = makeAtribuicaoArray(node->symbol, code);   break;     
+        default:                    result = tacJoin(tacJoin(tacJoin(code[0], code[1]), code[2]) , code[3]);
     }
     
     return result;
 }
 
-void tacPrintNode(TAC *node)
-{
-    if(node->type != TAC_SYMBOL)
-    {
+void tacPrintNode(TAC *node){
+    if(node->type != TAC_SYMBOL){
         printf("TAC - ");
         
-        switch(node->type)
-        {
+        switch(node->type){
             case TAC_SYMBOL:    printf("TAC_SYMBOL          "); break;
                 
             case TAC_ADD:
@@ -253,33 +222,27 @@ void tacPrintNode(TAC *node)
     }
 }
 
-TAC* makeAtribuicaoArray(HASH_NODE* res, TAC **code)
-{
+TAC* makeAtribuicaoArray(HASH_NODE* res, TAC **code){
     TAC *newTac = tacCreate(TAC_ATRIBUICAO_ARRAY, res, code[0] ? code[0]->res : 0, code[1] ? code[1]->res : 0);
     return tacJoin(code[0], tacJoin(code[1], newTac));
 }
 
-TAC* makeOperacaoBinaria(int operation, TAC** code)
-{
-    
+TAC* makeOperacaoBinaria(int operation, TAC** code){
     return tacJoin(code[0], tacJoin(code[1], tacCreate( operation, makeTemp(), code[0]->res ? code[0]->res : 0, code[1]->res ? code[1]->res : 0 )));
 }
 
-TAC* makeAtribuicao(HASH_NODE* res, TAC **code)
-{
+TAC* makeAtribuicao(HASH_NODE* res, TAC **code){
     TAC *newTac = tacCreate(TAC_ATRIBUICAO, res, code[0] ? code[0]->res : 0, 0);
     return tacJoin(code[0], newTac);
 }
 
-TAC* makeReturn(TAC **code)
-{
+TAC* makeReturn(TAC **code){
     TAC *expressao = code[0];
     TAC *tacReturn = tacCreate(TAC_RETURN, 0, expressao?expressao->res : 0, 0);
     return tacJoin(expressao, tacReturn);
 }
 
-TAC* makeWhenThen(TAC** code)
-{
+TAC* makeIfThen(TAC** code){
     TAC* expression;
     TAC* thenCmd;
     HASH_NODE *newLabel;
@@ -289,8 +252,7 @@ TAC* makeWhenThen(TAC** code)
     return tacJoin(tacJoin(tacJoin(code[0], expression), code[1]), thenCmd);
 }
 
-TAC* makeWhenThenElse(TAC** code)
-{
+TAC* makeIfThenElse(TAC** code){
     TAC *expression = code[0];
     TAC *thenCmd = code[1];
     TAC *elseCmd = code[2];
@@ -300,47 +262,33 @@ TAC* makeWhenThenElse(TAC** code)
     
     HASH_NODE *labelElseCmd = makeLabel();
     TAC* tacLabelElseCmd = tacCreate(TAC_LABEL, labelElseCmd, 0, 0);
-    
-    /* VERDADEIRO CONTINUA, FALSO VAI PRA LABELTHENCMD */
     TAC *tacIfz = tacCreate(TAC_IFZ, labelThenCmd, expression ? expression->res : 0, 0);
-    
-    /* VERDADEIRO FAZ O JUMP */
     TAC *tacJump = tacCreate(TAC_JUMP, labelElseCmd, 0, 0);
     
-    return tacJoin(expression,
-                   tacJoin(tacIfz,
-                           tacJoin(thenCmd,
-                                   tacJoin(tacJump,
-                                           tacJoin(tacLabelThenCmd,
-                                                   tacJoin(elseCmd,
-                                                           tacLabelElseCmd))))));
+    return tacJoin(expression,tacJoin(tacIfz,tacJoin(thenCmd,tacJoin(tacJump,tacJoin(tacLabelThenCmd,tacJoin(elseCmd,tacLabelElseCmd))))));
 }
 
-TAC* makeFnCall(AST_NODE *functionCall)
-{
+TAC* makeFnCall(AST_NODE *functionCall){
     AST_NODE *functionDefinition = findFuncDeclaration(functionCall);
     AST_NODE *expressoes = functionCall->sons[0];
     AST_NODE *args = functionDefinition->sons[1];
     
-    TAC *tacArgs = makeArgumentos(args, expressoes);
+    TAC *tacArgs = makeArguments(args, expressoes);
     TAC *tacCall = tacCreate(TAC_CALL, makeTemp(), functionCall->symbol, NULL);
     
     return tacJoin(tacArgs, tacCall);
 }
 
-TAC* makeFnDef(HASH_NODE *identifier, TAC** code)
-{
+TAC* makeFnDef(HASH_NODE *identifier, TAC** code){
     TAC *fnBody = code[2];
     TAC *fbBegin = tacCreate(TAC_BEGIN_FUNCTION, identifier, 0, 0);
     TAC *fbEnd = tacCreate(TAC_END_FUNCTION, identifier, 0, 0);
     return tacJoin(fbBegin, tacJoin(fnBody, fbEnd));
 }
 
-TAC *makeArgumentos(AST_NODE *args, AST_NODE *expressoes)
-{
+TAC *makeArguments(AST_NODE *args, AST_NODE *expressoes){
     TAC *tacListOfArgs = 0;
-    while(args && expressoes)
-    {
+    while(args && expressoes){
         HASH_NODE *argSymbol = args->sons[0]->symbol;
         AST_NODE *expr = expressoes->sons[0];
         
@@ -358,20 +306,17 @@ TAC *makeArgumentos(AST_NODE *args, AST_NODE *expressoes)
     return tacListOfArgs;
 }
 
-TAC* makeVariavel(HASH_NODE* res, TAC **code)
-{
+TAC* makeVariavel(HASH_NODE* res, TAC **code){
     TAC *newTac = tacCreate(TAC_VAR, res, code[1] ? code[1]->res : 0, 0);
     return tacJoin(code[1], newTac);
 }
 
-TAC* makeArrayInicializado(HASH_NODE* res, TAC **code)
-{
+TAC* makeArrayInicializado(HASH_NODE* res, TAC **code){
     TAC *newTac = tacCreate(TAC_ARRAY_DECLARADO, res, 0, 0);
     return tacJoin(newTac, code[1]);
 }
 
-TAC* makeArray(HASH_NODE* res, TAC **code)
-{
+TAC* makeArray(HASH_NODE* res, TAC **code){
     TAC *arrayTac = tacCreate(TAC_ARRAY_NAO_DECLARADO, res, code[1] ? code[1]->res : 0, code[2] ? makeLabelArray(res) : 0);
     if(code[2])
         return tacJoin(code[1], tacJoin(arrayTac, code[2]));
@@ -379,21 +324,18 @@ TAC* makeArray(HASH_NODE* res, TAC **code)
     return tacJoin(code[1], arrayTac);
 }
 
-TAC* makeRead(HASH_NODE *identifier)
-{
+TAC* makeRead(HASH_NODE *identifier){
     return tacCreate(TAC_READ, 0, identifier ? identifier : 0, 0);
 }
 
-TAC* makePrint(TAC **code)
-{
+TAC* makePrint(TAC **code){
     TAC *tacExpression = code[0];
     TAC *tacPrintList = code[1];
     
     if(!tacExpression)
         return 0;
     
-    if(!tacPrintList)
-    {
+    if(!tacPrintList){
         TAC *tacPrint = tacCreate(TAC_PRINT, 0, tacExpression ? tacExpression->res : 0, 0);
         return tacJoin(tacExpression, tacPrint);
     }
@@ -402,47 +344,7 @@ TAC* makePrint(TAC **code)
     return tacJoin(tacExpression, tacJoin(tacPrint, tacPrintList));
 }
 
-/* FOR (P = Q TO R) S */
-TAC* makeForTo(HASH_NODE* res, TAC **code)
-{
-    TAC *tacExpressionFinalValue = code[1];
-    TAC *tacLoopCmd = code[2];
-    
-    // P = Q
-    TAC *tacAttr = makeAtribuicao(res, code);
-    
-    // ANTES DE R
-    HASH_NODE *labelBeforeForToCondition = makeLabel();
-    TAC *tacLabelBeforeForToCondition = tacCreate(TAC_LABEL, labelBeforeForToCondition, 0, 0);
-    
-    // P <= R ?
-    TAC *tacCondition = tacCreate(TAC_LE, makeTemp(), res, tacExpressionFinalValue ? tacExpressionFinalValue->res : 0);
-    
-    /* INCREMENTO DO P */
-    TAC *tacIncrement = tacCreate(TAC_INCREMENT, res, 0, 0);
-    
-    /* O QUE VEM DEPOIS DO S */
-    HASH_NODE *labelAfterForToLoop = makeLabel();
-    TAC *tacLabelAfterForToLoop = tacCreate(TAC_LABEL, labelAfterForToLoop, 0, 0);
-    
-    /* SE P MAIOR QUE R ENTÃO VAI PRA DEPOIS DE S (O QUE ESTÁ FORA DO LOOP */
-    TAC *tacIfz = tacCreate(TAC_IFZ, labelAfterForToLoop, tacCondition ? tacCondition->res : 0, 0);
-    
-    /* CONTINUAÇÃO DO LOOP */
-    TAC *tacJumpToBeginOfForTo = tacCreate(TAC_JUMP, labelBeforeForToCondition, 0, 0);
-    
-    return tacJoin(tacAttr,
-                   tacJoin(tacLabelBeforeForToCondition,
-                           tacJoin(tacCondition,
-                                   tacJoin(tacIfz,
-                                           tacJoin(tacLoopCmd,
-                                                   tacJoin(tacIncrement,
-                                                           tacJoin(tacJumpToBeginOfForTo, tacLabelAfterForToLoop)))))));
-}
-
-/* WHILE (P) Q */
-TAC* makeWhile(TAC **code)
-{
+TAC* makeWhile(TAC **code){
     TAC* tacExpression = code[0];
     TAC* tacCommand = code[1];
     
@@ -456,10 +358,5 @@ TAC* makeWhile(TAC **code)
     
     TAC *tacJumpBack = tacCreate(TAC_JUMP, labelBeforeP, 0, 0);
     
-    return tacJoin(tacLabelBeforeP,
-                   tacJoin(tacExpression,
-                           tacJoin(tacIfz,
-                                   tacJoin(tacCommand,
-                                           tacJoin(tacJumpBack,
-                                                   tacLabelAfterQ)))));
+    return tacJoin(tacLabelBeforeP,tacJoin(tacExpression,tacJoin(tacIfz,tacJoin(tacCommand,tacJoin(tacJumpBack,tacLabelAfterQ)))));
 }
